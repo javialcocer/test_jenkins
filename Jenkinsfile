b3eb816b742829c61cb43c4d9e3e8e91b2e4021b
Javier Alcocer <augustborn1992@gmail.com>
2019-02-25 17:52:55 -0500

    test 2

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,11 +1,11 @@
 #!groovy
 // Jenkinsfile
 
-def DOCKER_HUB_USER="etejeda"
-def CONTAINER_NAME="dockerxc"
-def CONTAINER_TAG="beta"
+def DOCKER_HUB_USER="fjalcr"
+def CONTAINER_NAME="dockerTest"
+def CONTAINER_TAG="test1"
 def HTTP_PORT="8081"
-def NAMEJOB="XCARET.COM"
+def NAMEJOB="TEST"
 def WORKDIRLOCAL="/var/lib/docker/volumes/jenkins-data/_data/workspace/${NAMEJOB}"
 // Test 3
 pipeline {
@@ -24,16 +24,24 @@ pipeline {
             stage('Prepare Images') {
                 steps {
                     echo 'Run Docker Apache...'
+                    runApp(CONTAINER_NAME, CONTAINER_TAG, DOCKER_HUB_USER, HTTP_PORT, WORKDIRLOCAL)
+                    echo 'Run Selenium Hub in the network grid...'
+                    sh 'docker run --rm -d -p 4444:4444 --net grid --name selenium-hub-testing-1 selenium/hub'
+                    echo 'Run a node of firefox... (Selenium Grid)'
+                    sh 'docker run --rm -d --net grid --name node-firefox-1 -e HUB_HOST=selenium-hub-testing-1 -e START_XVFB=false -v /dev/shm:/dev/shm selenium/node-firefox'
                 }
             }
             stage('Build') {
                 steps {
                     echo 'Start Install Dependencies...'
+                    sh "docker run --rm --name npm-pipeline -v ${WORKDIRLOCAL}:/usr/src/app -w /usr/src/app node:8-alpine npm install"
+
                 }
             }
             stage('Test') {
                 steps {
                     echo 'Start Testing (Mocha)...'
+                    sh "docker run --rm --name npm-pipeline -v ${WORKDIRLOCAL}:/usr/src/app -w /usr/src/app node:8-alpine npm test"
 
                 }
             }
@@ -45,5 +53,31 @@ pipeline {
 
 
         }
+        post {
+            always {
+                echo '===== ALWAYS ====='
+                imagePrune(CONTAINER_NAME)
+            }
+        //    cleanup {
+        //           echo 'One way or another, I have finished'
+                   //deleteDir() /* clean up our workspace */
+        //       }
+        }
 
+}
+
+def imagePrune(containerName){
+    try {
+        print 'Stop all services..'
+        sh "docker stop $containerName"
+        sh "docker stop node-firefox-1"
+        sh "docker stop selenium-hub-testing-1"
+
+    } catch(error){}
+}
+
+def runApp(containerName, tag, dockerHubUser, httpPort, WORKSPACE){
+    sh "docker pull $dockerHubUser/$containerName:$tag"
+    sh "docker run -d --rm -p $httpPort:80 --name $containerName -v $WORKSPACE:/var/www/localhost/htdocs/ $dockerHubUser/$containerName:$tag"
+    echo "Application started on port: $httpPort (http)"
 }
---
+++ test/DefaultTest.js
@@ -0,0 +1,87 @@
+
+// const chrome = require('selenium-webdriver/chrome'); Ejemplo
+const { expect } = require('chai');
+const screen = {
+  width: 1280,
+  height: 720
+};
+const {Builder, By, Capabilities, Key, until} = require('selenium-webdriver');
+
+let firefox = require('selenium-webdriver/firefox');
+const VERSIONS = ['65.0'];
+let driver = new Builder().forBrowser('firefox')
+            .withCapabilities(Capabilities.firefox().setBrowserVersion('65.0'))
+            .usingServer('http://b7611845.ngrok.io:4444/wd/hub') // Conectamos a nuestro enjambre de navegadores listos para ejecutar nuestras tareas
+            .setFirefoxOptions(
+                new firefox.Options().headless()) // Activamos firefox en modo headless (Sin necesidad de interfaz grafica)
+            .build();
+
+var result = '';
+var tester = function(){
+ const resources = window.performance.getEntriesByType("resource");
+   var i = 0;
+   var list = new Array();
+   resources.forEach(function (resource) {
+       var str = resource.name;
+       var res = str.split("/");
+       if (res[2] == 'www.bexcaret.com') {
+         list.push(res[2]);
+       }
+   });
+   if (list.length > 0) {
+      return "prod";
+   }else{
+     return "noprod";
+   }
+}
+describe('Test XCARET.COM - v1', () => {
+
+    it('Vericar Telefonos - Footer - XCARET.COM - ESPAÑOL', async () => { // Titulo de nuestro Test
+
+        // Nuestra estructura de datos con los telefonos que deben de ir
+        let arrayNumeros = [
+            "Cancún: (01 998) 883-3143",
+            "Playa del Carmen: (01 984) 206-0038",
+            "USA-CAN: 1-855-326-0682",
+            "Brasil: 0-800-892-3371",
+            "Argentina: 0800-122-0384",
+            "Colombia: 01-800-952-0705",
+            "España: 900-965-224",
+            "Chile: 0-800-835-016"
+        ];//prueba
+        await driver.get('http://local.jenkinsdev.com/'); // Recuerden que la direccion seria local ya que nuestro contenedor es desplegado dentro de la red local
+
+        // Buscamos gracias al webdriver que nos otorga selenium entre los distintos elementos que conforma nuestra web
+        await driver.wait(until.elementLocated(By.className('phones')));
+        let links = await driver.findElements({css:'#sales > ul.phones > li > a'});
+        let flag = false;
+        for(let link of links) {
+             text = await link.getText();
+             console.log(text);
+             if(!arrayNumeros.includes(text)){
+                flag = true; // Buscamos telefono por telefono si dentro de nuestra estructura encontramos una diferencia con lo que esta en el codigo levantamos nuestro flag como true
+             }
+         }
+        expect(flag).to.equal(false); //Nosotros esperamos que el flag este en falso, osea si esta en TRUE tenemos UN PROBLEMA
+
+    });
+
+    it('Verificar Etiqueta SEO - XCARET.COM', async () => {
+         await driver.get('http://local.jenkinsdev.com/');
+         // await driver.sleep(20000);
+         const title = await driver.findElement(By.name('robots')).getAttribute('content');
+         expect(title).to.equal('index, follow');
+     });
+
+     it('Verificar Carrito - XCARET.COM', async () => {
+         await driver.get('https://www.xcaret.com/es/');
+         //await driver.sleep(20000);
+       let v = await driver.executeScript(tester);
+       expect(v).to.equal("prod");
+     });
+
+
+
+
+    after(async () => driver.quit());
+});
